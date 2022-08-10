9a609fdce32f4307c18f69c0d2d91fb89fc3971b
ignaciothompson <chonathomp@gmail.com>
2022-08-10 16:59:13 -0300

    pagina Autos

--- js/products.js
+++ js/products.js
@@ -1,11 +1,47 @@
-let url = 'https://japceibal.github.io/emercado-api/cats_products/101.json'
+//const AUTOS_URL= "https://japceibal.github.io/emercado-api/cats_products/101.json";
+
+//let data = []
+//fetch(AUTOS_URL)
+    //.then((response) => response.json())
+    //.then((data) => console.log(data))
+
+let categoriesArray = [];
+
+function mostrarProductos(array){
+    let htmlContentToAppend = "";
+    for(let i = 0; i < array.products.length; i++){ 
+        let category = array.products[i];
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + category.image + `" alt="product image" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <div class="mb-1">
+                        <h4>`+ category.name + " - " + category.currency + " " + category.cost +`</h4> 
+                        <p> `+ category.description +`</p> 
+                        </div>
+                        <small class="text-muted">` + category.soldCount + ` artículos</small> 
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        `
+        document.getElementById("info-productos").innerHTML = htmlContentToAppend; 
+    }
+}
 
 
-async function CargarProductos(){
-    const response = await fetch(url);
-    const info = await response.json();
 
-    console.log(info)
-} 
-
-
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(AUTOS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            categoriesArray = resultObj.data;
+            mostrarProductos(categoriesArray);
+        }
+    });
+});
